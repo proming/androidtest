e1b790fa3ba72dd2cbb6f349d9b2e86d2590156a
proming <promingx@gmail.com>
2009-06-17 19:59:41 +0800

    Test

---
+++ src/com/longming/androidtest/androidtest.java
@@ -0,0 +1,13 @@
+package com.longming.androidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class androidtest extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
